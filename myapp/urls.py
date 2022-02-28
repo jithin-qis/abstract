ba1e208d164d515ecd4b647a6c887015254bddb9
jithin-qis <jithin.qis@gmail.com>
2022-02-28 23:14:42 +0530

    initial

---
+++ myapp/urls.py
@@ -0,0 +1,8 @@
+import imp
+from unicodedata import name
+from django.urls import path 
+from .views import *
+
+urlpatterns = [ 
+    path('', index, name='index'),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import *
 
+def index(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = ContactForm()
+    return render(request, 'myapp/form1.html', {'form': form})
